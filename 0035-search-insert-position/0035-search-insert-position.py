9e648010ed606d4e61257a0513c9ae3d373f4df7
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-03 16:40:10 -0700

    Time: 44 ms (86.42%), Space: 17.2 MB (97.15%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -19,31 +19,3 @@ class Solution:
                 l = mid + 1
                     
         return ans
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # l=0
-        # r=len(nums)-1
-        # while r>=l:
-        #     mid=(r+l)//2
-        #     if nums[mid]==target:
-        #         return mid
-        #     elif nums[mid]<target:
-        #         l=mid+1
-        #     else:
-        #         r=mid-1
-        # return r+1    
-
-        
-        
