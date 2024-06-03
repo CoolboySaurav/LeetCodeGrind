e608693bba7b4f2d8784e877b48a34df8d620d3b
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-03 14:17:44 -0700

    Time: 183 ms (30.41%), Space: 12.8 MB (55.66%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        l=mid=0
+        r=len(nums)-1
+        while r>=l:
+            mid=(l+r)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                r=mid-1
+            else:
+                l=mid+1
+        return -1
