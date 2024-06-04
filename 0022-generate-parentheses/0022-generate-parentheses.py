df99e5c57af01811bd344b1f9780e3e33d103f87
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-04 16:07:25 -0700

    Time: 18 ms (51.08%), Space: 11.7 MB (98.83%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -28,33 +28,3 @@ class Solution(object):
         
         
         
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-#         if n==1:
-#             return ["()"]
-#         res=[]
-#         def generate(countOpen,countClose,path):
-#             if countOpen==countClose==n:
-#                 res.append(path)
-#                 return
-#             if countOpen<=n and countClose<n and countOpen>countClose:
-#                 generate(countOpen+1,countClose,path+"(")
-#                 generate(countOpen,countClose+1,path+")")    
-#             elif countOpen<=n and countClose<n and countOpen<=countClose:
-#                 generate(countOpen+1,countClose,path+"(")
-        
-#         generate(0,0,"")
-#         return res
