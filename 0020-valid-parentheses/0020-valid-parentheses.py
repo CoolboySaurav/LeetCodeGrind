ca46b6be0ec32cd8c98a4e60240a28b67421bfcc
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-04 15:46:23 -0700

    Time: 21 ms (16.54%), Space: 11.8 MB (23.60%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -23,56 +23,6 @@ class Solution(object):
                         return False
                 else:
                     return False
-        return not stack
-                
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        # stack=[]
-        # # end=[")","}","]"]
-        # # start=["(","[","{"]
-        # parent={"(":")","[":"]","{":"}"}
-        # for par in s:
-        #     if par not in parent.values():
-        #         stack.append(par)
-        #     else:
-        #         if stack:
-        #             val=stack.pop()
-        #             if par==parent[val]:
-        #                 continue
-        #             else:
-        #                 return False
-        #         else:
-        #             return False
-        # if stack:
-        #     return False
-        # else:
-        #     return True
-                
+        return not stack   
                 
             
