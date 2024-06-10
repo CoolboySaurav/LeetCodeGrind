6731ce0791068023f9e17736143ccb01bed23367
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-10 02:01:49 -0700

    Time: 7 ms (99.71%), Space: 12.1 MB (43.27%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -32,35 +32,53 @@ class Solution(object):
         self.adjecency(root,parentMap)
         
         # BFS call for k direction from target
-        q = deque()
-        q.append(target)
-        visited = set()
-        visited.add(target)
-        currDepth = 0
+#         q = deque()
+#         q.append(target)
+#         visited = set()
+#         visited.add(target)
+#         currDepth = 0
         
-        while q:
-            L = len(q)
-            if currDepth == k:
-                break
-            currDepth += 1
+#         while q:
+#             L = len(q)
+#             if currDepth == k:
+#                 break
+#             currDepth += 1
             
-            for i in xrange(L):
-                node = q.popleft()
-                if node.left and node.left not in visited:
-                    q.append(node.left)
-                    visited.add(node.left)
-                if node.right and node.right not in visited:
-                    q.append(node.right)
-                    visited.add(node.right)
-                if node in parentMap and parentMap[node] not in visited:
-                    q.append(parentMap[node])
-                    visited.add(parentMap[node])
+#             for i in xrange(L):
+#                 node = q.popleft()
+#                 if node.left and node.left not in visited:
+#                     q.append(node.left)
+#                     visited.add(node.left)
+#                 if node.right and node.right not in visited:
+#                     q.append(node.right)
+#                     visited.add(node.right)
+#                 if node in parentMap and parentMap[node] not in visited:
+#                     q.append(parentMap[node])
+#                     visited.add(parentMap[node])
         
+#         res = []
+        
+#         while q:
+#             node = q.popleft()
+#             res.append(node.val)
+        
+#         return res
+        
+    
+        q = deque()
+        q.append([target,0])
+        visited = set([target])
         res = []
         
         while q:
-            node = q.popleft()
-            res.append(node.val)
+            node, dist = q.popleft()
+            
+            if dist == k:
+                res.append(node.val)
+            
+            for neighbor in [node.left, node.right, parentMap.get(node)]:
+                if neighbor and neighbor not in visited:
+                    q.append([neighbor, dist + 1])
+                    visited.add(neighbor)
         
         return res
-        
