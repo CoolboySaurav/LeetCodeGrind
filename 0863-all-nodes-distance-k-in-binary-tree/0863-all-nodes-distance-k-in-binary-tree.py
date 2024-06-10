1084e99e3ae8600f5b4f15433ac548ed393a9014
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-10 01:57:34 -0700

    Time: 11 ms (95.91%), Space: 12 MB (72.22%) - LeetHub

---
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -0,0 +1,66 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def adjecency(self, root, parentMap):
+        q= deque()
+        q.append(root)
+        
+        while q:
+            node =q.popleft()
+            
+            if node.left:
+                parentMap[node.left] = node
+                q.append(node.left)
+            if node.right:
+                parentMap[node.right] = node
+                q.append(node.right)        
+        
+        
+    def distanceK(self, root, target, k):
+        """
+        :type root: TreeNode
+        :type target: TreeNode
+        :type k: int
+        :rtype: List[int]
+        """
+        parentMap = {}
+        self.adjecency(root,parentMap)
+        
+        # BFS call for k direction from target
+        q = deque()
+        q.append(target)
+        visited = set()
+        visited.add(target)
+        currDepth = 0
+        
+        while q:
+            L = len(q)
+            if currDepth == k:
+                break
+            currDepth += 1
+            
+            for i in xrange(L):
+                node = q.popleft()
+                if node.left and node.left not in visited:
+                    q.append(node.left)
+                    visited.add(node.left)
+                if node.right and node.right not in visited:
+                    q.append(node.right)
+                    visited.add(node.right)
+                if node in parentMap and parentMap[node] not in visited:
+                    q.append(parentMap[node])
+                    visited.add(parentMap[node])
+        
+        res = []
+        
+        while q:
+            node = q.popleft()
+            res.append(node.val)
+        
+        return res
+        
