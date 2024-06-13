eddd99c4e197afad3d0b274b6adb97900af1b6a6
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-13 16:51:05 -0700

    Time: 69 ms (45.87%), Space: 14.9 MB (19.33%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.py
@@ -0,0 +1,103 @@
+class Solution(object):
+    def canFinish(self, n, pre):
+        """
+        :type numCourses: int
+        :type prerequisites: List[List[int]]
+        :rtype: bool
+        """
+        
+        def dfs(node, visit, pathVisit):
+            visit[node] = 1
+            pathVisit[node] = 1
+            
+            for i in adj[node]:
+                if not visit[i]:
+                    if dfs(i, visit, pathVisit):
+                        return True
+                elif pathVisit[i]:
+                    return True
+            
+            pathVisit[node] = 0
+            return False
+        
+        
+        
+        
+        
+        visit = [0]*n
+        pathVisit = [0]*n
+        adj = { i:[] for i in range(n)}
+        
+        for crs, prereq in pre:
+            adj[crs].append(prereq)
+        
+        
+        for i in xrange(n):
+            if not visit[i]:
+                if dfs(i, visit, pathVisit):
+                    return False
+        
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+# NEETCODE DFS with higher optimization
+
+        # visit=set()
+        # preCourse={i:[] for i in range(numCourses)}
+        # for crs,pre in prerequisites:
+        #     preCourse[crs].append(pre)
+
+        # def dfs(crs):
+        #     if crs in visit:
+        #         return False
+        #     if not preCourse[crs]:
+        #         return True
+        #     visit.add(crs)
+        #     for pre in preCourse[crs]:
+        #         if dfs(pre)==False:
+        #             return False
+        #     visit.remove(crs)
+        #     preCourse[crs]=[]
+        #     return True
+
+        
+        # for crs in range(numCourses):
+        #     if dfs(crs)==False:
+        #         return False
+        # return True
+
+# BFS with higher space complexity
+#         preCourse={i:[] for i in range(numCourses)}
+#         for crs,pre in prerequisites:
+#             preCourse[crs].append(pre)
+#         indegree=[0]*len(preCourse)
+#         for i in preCourse:
+#             for j in preCourse[i]:
+#                 indegree[j]+=1
+
+#         q=deque() 
+#         for i in range(numCourses):
+#             if indegree[i]==0:
+#                 q.append(i)
+#         res=[]
+#         while q:
+#             node=q.popleft()
+#             res.append(node)
+#             for i in preCourse[node]:
+#                 indegree[i]-=1
+#                 if indegree[i]==0:
+#                     q.append(i)
+#         return len(res)==numCourses
