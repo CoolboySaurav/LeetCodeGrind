252d5de4d6f15d3f29c0486af104363667b8e335
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-17 19:16:50 -0700

    Time: 1015 ms (5.00%), Space: 21 MB (13.45%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -4,18 +4,29 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        maxi=0
-        i,j=0,1
-        profit=0
-        # while j>=0:
-        #     profit=max(profit,0)
-        #     profit-=
-        for p in range(1,len(prices)):
-            j=p
-            if prices[j]<prices[i]:
-                i=p
-            elif prices[j]>prices[i]:
-                maxi=max(maxi,prices[j]-prices[i])
+        
+        maxProfit = -1e7
+        left = 0
+        for i in range(len(prices)):
+            if prices[i] >= prices[left]:
+                maxProfit = max(maxProfit, (prices[i]-prices[left]))
+            else:
+                left = i 
+        return maxProfit
+        
+        
+        # maxi=0
+        # i,j=0,1
+        # profit=0
+        # # while j>=0:
+        # #     profit=max(profit,0)
+        # #     profit-=
+        # for p in range(1,len(prices)):
+        #     j=p
+        #     if prices[j]<prices[i]:
+        #         i=p
+        #     elif prices[j]>prices[i]:
+        #         maxi=max(maxi,prices[j]-prices[i])
 
 
 
@@ -33,4 +44,4 @@ class Solution(object):
         #         maxi=max(maxi,profit)
         #     i+=1
         #     j-=1
-        return maxi
+        #return maxi
