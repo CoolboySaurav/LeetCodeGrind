3ee3daaff6572196662a9e40a994482bedc5707e
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-20 14:41:00 -0700

    Time: 84 ms (91.00%), Space: 12.4 MB (73.63%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def minWindow(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: str
+        """
+        
+        start = -1 
+        minLen = float(1e8)
+        count = 0
+        l = r = 0
+        
+        char = defaultdict(int)
+        
+        for i in t:
+            char[i] += 1
+            
+        while r < len(s):
+            if char[s[r]] > 0:
+                count += 1
+            char[s[r]] -= 1
+            
+            while count == len(t):
+                if (r - l + 1) < minLen:
+                    minLen = r - l + 1 
+                    start = l
+                char[s[l]] += 1
+                if char[s[l]] > 0:
+                    count -= 1
+                l += 1
+            r += 1
+        
+        return "" if start == -1 else s[start: start + minLen]
