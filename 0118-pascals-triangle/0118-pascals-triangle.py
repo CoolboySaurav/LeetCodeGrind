4266ca7bad72d6f315b25493931835f282cc736c
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-22 17:48:36 -0700

    Time: 20 ms (23.44%), Space: 11.6 MB (31.65%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -7,18 +7,16 @@ class Solution(object):
         res= []
         i = 1
         res.append([1])
-        while i < numRows:
-            
-            level = []
-            
-            for j in range(i+1):
+        
+        while i < numRows:            
+            level = []           
+            for j in xrange(i+1):
                 a = 0
                 b = 0
                 if j > 0:
                     a = res[i-1][j-1]
                 if j <= i - 1:
                     b = res[i-1][j]
-                
                 level.append(a+b)
             res.append(level)
             i += 1
