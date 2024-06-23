af6042add3a5a085bfef76f75d9c25128a119d49
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-22 23:39:30 -0700

    Time: 2796 ms (14.73%), Space: 12.4 MB (96.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -21,18 +21,32 @@ class Solution(object):
     
         # Memoization
         
-        dp = [-1]*n
+#         dp = [-1]*n
         
-        def jump(ind):
-            if ind>= n:
-                return 0
-            if dp[ind] != -1:
-                return dp[ind]
+#         def jump(ind):
+#             if ind>= n:
+#                 return 0
+#             if dp[ind] != -1:
+#                 return dp[ind]
+#             minJump = float('inf')
+#             for i in range(ind+1, ind+nums[ind]+1):
+#                 minJump = min(minJump, 1 + jump(i))
+#             dp[ind] = minJump
+#             return minJump
+        
+#         return jump(0)
+    
+        # Tabulaton Approach
+        
+        dp = [float('inf')]*(n+1)
+        dp[n] = 0
+        
+        for i in xrange(n-1,-1,-1):
             minJump = float('inf')
-            for i in range(ind+1, ind+nums[ind]+1):
-                minJump = min(minJump, 1 + jump(i))
-            dp[ind] = minJump
-            return minJump
+            for j in xrange(i + 1, i + nums[i]+1):
+                if j <= n:
+                    minJump = min(minJump, 1 + dp[j])
+            dp[i] = minJump
         
-        return jump(0)
+        return dp[0]
         
