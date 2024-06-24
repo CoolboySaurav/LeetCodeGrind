60af541d43f79da97db922808d15ce0cd429015b
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-23 20:16:24 -0700

    Time: 88 ms (57.65%), Space: 12.5 MB (70.11%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.py
+++ 0045-jump-game-ii/0045-jump-game-ii.py
@@ -5,6 +5,24 @@ class Solution(object):
         :rtype: int
         """
         n = len(nums) - 1
+        
+        # Striver- Greedy
+        
+        jumps = 0
+        l = r = 0
+        
+        while r < n:
+            farthest = 0
+            
+            for i in xrange(l, r+1):
+                farthest = max( farthest, i + nums[i])
+            l = r + 1
+            r = farthest
+            jumps += 1
+        
+        return jumps
+        
+        
     
         # Tabulaton Approach
         
