3f87886ad12db5da715b6c6af481f0c2865209e7
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-23 19:38:58 -0700

    Time: 361 ms (44.07%), Space: 12.7 MB (41.03%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -4,6 +4,19 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
+        # Striver - Greedy
+        maxInd = 0
+        
+        for i in xrange(len(nums)):
+            if i > maxInd:
+                return False
+            maxInd = max( maxInd, i + nums[i])
+        
+        return True
+        
+        
+        
+        
         #Greedy
         goal=len(nums)-1
         for i in range(len(nums)-2,-1,-1):
