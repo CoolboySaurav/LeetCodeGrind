be36f78b80ad509380dd6f223eca6a036d2c0d47
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-23 17:15:06 -0700

    Time: 198 ms (23.92%), Space: 22.3 MB (34.17%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -6,22 +6,19 @@ class Solution(object):
         :rtype: int
         """
         
-        if target == nums[0]:
-            return 1
-        
         l = r = 0
         minLen = float('inf')
-        curSum = 0
+        curSum = nums[0]
         
         while r < len(nums):
-            curSum += nums[r]
-            while curSum > target:
-                if curSum >= target:
-                    minLen = min(minLen, (r - l + 1))
+            if curSum >= target:
+                minLen = min( minLen, r - l + 1)
                 curSum -= nums[l]
                 l += 1
-            if curSum >= target:
-                minLen = min(minLen, (r - l + 1))    
-            r += 1
+            else:
+                r += 1
+                if r == len(nums):
+                    break
+                curSum += nums[r]
         
         return minLen if minLen != float('inf') else 0
