be4ad608be783b4269eaa1ef8a8441719ae51205
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-26 23:41:33 -0700

    Time: 19 ms (22.35%), Space: 12.3 MB (5.30%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -5,35 +5,65 @@ class Solution(object):
         :rtype: bool
         """
         
+        # Dynamic Programming
+        n = len(s)
+        dp = [[-1]*(n+1) for _ in xrange(n+1)]
         
-        # Stack solution
-        
-        openBracket = []
-        star = []
-        
-        for i in xrange(len(s)):
-            if s[i] == "(":
-                openBracket.append(i)
-            elif s[i] == "*":
-                star.append(i)
-            else:
-                if openBracket:
-                    openBracket.pop()
-                elif star:
-                    star.pop()
-                else:
-                    return False
-        
-        while openBracket:
-            if not star:
+        def solve(ind, openBracket):
+            
+            if openBracket < 0:
                 return False
-            elif openBracket and star:
-                if openBracket[-1] < star[-1]:
-                    openBracket.pop()
-                    star.pop()
+            
+            if ind == n:
+                if openBracket == 0:
+                    return True
                 else:
                     return False
                 
+            if dp[ind][openBracket] != -1:
+                return dp[ind][openBracket]
+            
+            if s[ind] == "(":
+                dp[ind][openBracket] = solve(ind + 1, openBracket + 1)
+                return dp[ind][openBracket]
+            elif s[ind] == ")":
+                dp[ind][openBracket] = solve(ind+1, openBracket - 1)
+                return dp[ind][openBracket]
+            else:
+                dp[ind][openBracket] = solve(ind+1, openBracket + 1) or solve(ind + 1, openBracket) or solve(ind+1, openBracket -1)
+                return dp[ind][openBracket]
         
-        return  True
+        return solve(0,0)
+        
+        
+        # Stack solution
+        
+#         openBracket = []
+#         star = []
+        
+#         for i in xrange(len(s)):
+#             if s[i] == "(":
+#                 openBracket.append(i)
+#             elif s[i] == "*":
+#                 star.append(i)
+#             else:
+#                 if openBracket:
+#                     openBracket.pop()
+#                 elif star:
+#                     star.pop()
+#                 else:
+#                     return False
+        
+#         while openBracket:
+#             if not star:
+#                 return False
+#             elif openBracket and star:
+#                 if openBracket[-1] < star[-1]:
+#                     openBracket.pop()
+#                     star.pop()
+#                 else:
+#                     return False
+                
+        
+#         return  True
         
