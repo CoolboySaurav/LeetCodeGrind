dbfa24dff566132e64a585f70ffd976b0e4eb2a2
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-06-26 20:25:39 -0700

    Time: 20 ms (50.26%), Space: 11.7 MB (42.27%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -18,13 +18,15 @@ class Solution(object):
             
             if ind == n:
                 if count==4:
-                    self.result.append(path[:-1])
+                    self.result.append(path[:-1]) # Remove last dot
                 return 
             
-            for i in range(1,4):
+            for i in range(1,4): # IP segments are at most 3 digits
                 if ind + i > len(s):
                     break
                 segment = s[ind:ind+i]
+                
+                # Check if the segment is valid or not
                 if (segment.startswith('0') and len(segment) >1) or  (i == 3 and int(segment) > 255):
                     continue
                 solve(ind + i, path + segment + ".", count + 1)
