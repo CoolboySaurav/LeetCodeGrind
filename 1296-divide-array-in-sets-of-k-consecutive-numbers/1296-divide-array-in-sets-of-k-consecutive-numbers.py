edcfcc700ed6d06eb56c2004bdd2ed2cbb55f31a
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-01 01:52:14 -0700

    Time: 362 ms (63.86%), Space: 25.1 MB (66.34%) - LeetHub

--- 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
+++ 1296-divide-array-in-sets-of-k-consecutive-numbers/1296-divide-array-in-sets-of-k-consecutive-numbers.py
@@ -1,17 +1,21 @@
 class Solution(object):
     def isPossibleDivide(self, nums, k):
         """
-        :type nums: List[int]
-        :type k: int
-        :rtype: bool
+        
         """
         freq = collections.Counter(nums)
         
         for i in sorted(nums):
-            if freq[i] != 0:
+            if freq[i] != 0: # If count is 0 means element has been used in previous subsequence
                 f = freq[i]
-                for j in xrange(i, i + k):
-                    freq[j] -= f
+                for j in xrange(i, i + k): 
+                    freq[j] -= f # Optimization to quickly skill double counting in case of duplicates
                     if freq[j] < 0:
                         return False
+        """
+        For example, if certain element has 2 occurances, then its nearby elements must have exactly 2 occurances
+        for forming consecutive subsequence. Example: [1,2,3,3,4,4,5,6] here since 3 has 2 occurances, 4 has to have 
+        2 occurances to make it work
+        
+        """
         return True
