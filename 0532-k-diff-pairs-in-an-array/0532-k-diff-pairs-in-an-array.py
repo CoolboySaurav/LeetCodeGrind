65e304434b44bfa3a1b75cb1521185465d824341
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-02 15:09:14 -0700

    Time: 81 ms (33.53%), Space: 13.7 MB (60.59%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.py
@@ -5,6 +5,33 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # Binary Search Approach ( Slower but important approach to remember)
+        
+        def binarySearch(nums, start, target):
+            l, r = start, len(nums) - 1
+            
+            while l <= r:
+                mid = (l + r) // 2
+                
+                if nums[mid] == target:
+                    return True
+                elif nums[mid] > target:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            return False
+        
+        nums.sort()
+        count = 0
+        answer = set()
+        
+        for i in xrange(len(nums)):
+            if binarySearch(nums, i + 1, nums[i] + k):
+                answer.add((nums[i], nums[i] + k))
+        
+        return len(answer)
+        
+        
         # HashMap Solution
         numbers = collections.Counter(nums)
         count = 0
