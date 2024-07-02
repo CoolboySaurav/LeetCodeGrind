e4759b21ebb70589520ebde676d56d7fe987f6fd
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-01 20:07:25 -0700

    Time: 30 ms (64.74%), Space: 11.8 MB (84.66%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -5,22 +5,47 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: List[int]
         """
-        
-        num1idx = {n:i for i,n in enumerate(nums1)}
-        res = [-1]*len(nums1)
+        n = len(nums1)
+        res = [-1]*n
+        indexMap = {}
+        for i, n in enumerate(nums1):
+            indexMap[n] = i
         stack = []
         
-        for i in xrange(len(nums2)-1, -1,-1):
-            curr = nums2[i]
-            
-            while stack and stack[-1] <= curr:
+        for i in xrange(len(nums2) - 1, -1, -1):
+
+            while stack and nums2[i] > stack[-1]:
                 stack.pop()
-            if curr in num1idx:
-                if stack:
-                    res[num1idx[curr]] = stack[-1]
-                else:
-                    res[num1idx[curr]] = -1
             
-            stack.append(curr)
+            if nums2[i] in indexMap:
+                if stack:
+                    res[indexMap[nums2[i]]] = stack[-1]        
+            
+            stack.append(nums2[i])
         
         return res
+                
+                
+        
+        
+        
+        
+        
+#         num1idx = {n:i for i,n in enumerate(nums1)}
+#         res = [-1]*len(nums1)
+#         stack = []
+        
+#         for i in xrange(len(nums2)-1, -1,-1):
+#             curr = nums2[i]
+            
+#             while stack and stack[-1] <= curr:
+#                 stack.pop()
+#             if curr in num1idx:
+#                 if stack:
+#                     res[num1idx[curr]] = stack[-1]
+#                 else:
+#                     res[num1idx[curr]] = -1
+            
+#             stack.append(curr)
+        
+#         return res
