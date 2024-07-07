77ac8676edf904a961197b15eeec0be0f3d5a1a7
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-07 14:02:25 -0700

    Time: 40 ms (12.85%), Space: 11.7 MB (58.83%) - LeetHub

--- 0010-regular-expression-matching/0010-regular-expression-matching.py
+++ 0010-regular-expression-matching/0010-regular-expression-matching.py
@@ -6,6 +6,31 @@ class Solution(object):
         :rtype: bool
         """
         m, n = len(s), len(p)
+
+        #Tabulation
+        dp = [[False]*(n+1) for _ in xrange(m+1)]
+        dp[0][0] = True # Empty String matchs empty pattern
+        
+        
+        # Initialize pattern for "*"
+        for j in xrange(2,n+1):
+            if p[j-1] == "*":
+                dp[0][j] = dp[0][j-2]
+        
+        
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == s[i - 1] or p[j - 1] == '.':
+                    dp[i][j] = dp[i - 1][j - 1]
+                elif p[j - 1] == '*':
+                    dp[i][j] = dp[i][j - 2]  # Zero occurrence
+                    if p[j - 2] == s[i - 1] or p[j - 2] == '.':
+                        dp[i][j] = dp[i][j] or dp[i - 1][j]
+
+        return dp[m][n]
+        
+        
+        # Memoization         
         memo = {}
         
         def match(i,j):
