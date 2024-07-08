dbef713710ce495bd0a0c937760c416d9d320ae6
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-07 19:40:55 -0700

    Time: 14 ms (50.76%), Space: 11.6 MB (89.95%) - LeetHub

---
+++ 0394-decode-string/0394-decode-string.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    def decodeString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        res = ""
+        stack = [] 
+        
+        for i in s:
+            if i != "]":
+                stack.append(i)
+            else:
+                curStr = ""
+                while stack[-1] != "[":
+                    char = stack.pop()
+                    curStr = char + curStr
+                stack.pop()
+                
+                k = ""
+                while stack and stack[-1].isdigit():
+                    k = stack.pop() + k                    
+                stack.append(int(k) * curStr)
+            
+        return "".join(stack)
+            
