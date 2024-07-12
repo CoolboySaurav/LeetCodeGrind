6d0885461dd8787c7d2b14f39479afc480ea13b8
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-12 00:13:39 -0700

    Time: 317 ms (25.13%), Space: 22.3 MB (9.05%) - LeetHub

---
+++ 0398-random-pick-index/0398-random-pick-index.py
@@ -0,0 +1,26 @@
+class Solution:
+
+    def __init__(self, nums):
+        self.numToIdx = defaultdict(list)
+        for i, num in enumerate(nums):
+            self.numToIdx[num].append(i)
+
+
+
+
+        # self.num_idx = defaultdict(list)
+        # for i, num in enumerate(nums):
+        #     self.num_idx[num].append(i)
+
+    def pick(self, target):
+        return random.choice(self.numToIdx[target])
+
+
+
+        # return random.choice(self.num_idx[target])
+            
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.pick(target)
