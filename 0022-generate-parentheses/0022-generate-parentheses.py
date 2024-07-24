e6f3f0ec7ffa5dda9510736eb34cfad9dddf9db9
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-07-23 19:09:16 -0700

    Time: 26 ms (9.21%), Space: 11.9 MB (55.54%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -4,24 +4,40 @@ class Solution(object):
         :type n: int
         :rtype: List[str]
         """
-        if n == 1:
-            return ["()"]
-        
         res = []
-        
-        def generate(left,right,path):
-            if len(path) == n*2:
+        def helper(path, open, close):
+            if open == close == n:
                 res.append(path)
                 return 
             
-            if left < n:
-                generate(left+1,right,path+'(')
-            
-            if right<left:
-                generate(left, right+1, path+ ')')
-                
-        generate(0,0,'')
+            if open < n:
+                helper(path + "(", open + 1, close)
+            if open > close:
+                helper(path + ")", open, close + 1)
+        
+        helper("", 0, 0)
         return res
+        
+        
+        
+#         if n == 1:
+#             return ["()"]
+        
+#         res = []
+        
+#         def generate(left,right,path):
+#             if len(path) == n*2:
+#                 res.append(path)
+#                 return 
+            
+#             if left < n:
+#                 generate(left+1,right,path+'(')
+            
+#             if right<left:
+#                 generate(left, right+1, path+ ')')
+                
+#         generate(0,0,'')
+#         return res
         
         
         
