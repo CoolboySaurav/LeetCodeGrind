8a0fd02403041b75d0c97621e34aa80ca4a50967
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-09 20:41:32 -0700

    Time: 25 ms (52.08%), Space: 45 MB (42.19%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -4,18 +4,19 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int l = 1; // Minimum possible eating speed
         int r = findMax(piles); // Maximum possible eating speed
-
+        int ans = r;
         while (l <= r) {
             int mid = l + (r - l) / 2; // Middle speed to check
             
             if (canEatAll(piles, mid, h)) {
+                ans = mid;
                 r = mid - 1; // Try for a smaller speed
             } else {
                 l = mid + 1; // Increase speed
             }
         }
         
-        return l;
+        return ans;
     }
     
     private boolean canEatAll(int[] piles, int k, int h) {
