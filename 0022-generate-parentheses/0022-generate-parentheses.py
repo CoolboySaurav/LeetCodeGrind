b5f2e865e8b29b5e49f4fc0b087997bf924804ec
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-11 22:04:38 -0700

    Time: 42 ms (27.51%), Space: 16.9 MB (40.68%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,46 +1,19 @@
-class Solution(object):
-    def generateParenthesis(self, n):
-        """
-        :type n: int
-        :rtype: List[str]
-        """
-        res = []
-        def helper(path, open, close):
-            if open == close == n:
-                res.append(path)
-                return 
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        
+        result = []
+        
+        def helper(open, close, path):
+            #Base Case
+            if len(path) == 2*n:
+                if open == close:
+                    result.append(path)
+                    return 
             
             if open < n:
-                helper(path + "(", open + 1, close)
+                helper(open + 1, close, path + "(")
+            
             if open > close:
-                helper(path + ")", open, close + 1)
-        
-        helper("", 0, 0)
-        return res
-        
-        
-        
-#         if n == 1:
-#             return ["()"]
-        
-#         res = []
-        
-#         def generate(left,right,path):
-#             if len(path) == n*2:
-#                 res.append(path)
-#                 return 
-            
-#             if left < n:
-#                 generate(left+1,right,path+'(')
-            
-#             if right<left:
-#                 generate(left, right+1, path+ ')')
-                
-#         generate(0,0,'')
-#         return res
-        
-        
-        
-        
-        
-        
+                helper(open, close + 1, path + ")")
+        helper(0,0,"")
+        return result
