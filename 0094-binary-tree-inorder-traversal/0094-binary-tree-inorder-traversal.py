3581fa70d5c67db224c219e8d8c8465d885853d6
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-13 22:50:05 -0700

    Time: 32 ms (76.27%), Space: 16.4 MB (91.80%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.py
@@ -1,47 +1,18 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def inorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        
-        # Recursion Solution
-        
-#         if not root:
-#             return None
-        
-#         res = []
-        
-#         def travel(node):
-#             if not node:
-#                 return 
-            
-#             travel(node.left)
-#             res.append(node.val)
-#             travel(node.right)
-        
-#         travel(root)
-#         return res
-        
-        
-        # Iterative Solution
-        
-        res=[]
-        stack=[]
-        curr=root
-        while stack or curr:
-            while curr:
-                stack.append(curr)
-                curr=curr.left
-            curr=stack.pop()
-            res.append(curr.val)
-            curr=curr.right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st = []
+        cur = root
+        res = []
+        while st or cur:
+            while cur:
+                st.append(cur)
+                cur = cur.left
+            res.append(st[-1].val)
+            cur = st.pop().right
         return res
-
-
