94207e718a1e71a799063e8e920d222d6967ca39
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-13 22:38:51 -0700

    Time: 34 ms (65.27%), Space: 16.4 MB (92.56%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.py
@@ -1,64 +1,19 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def preorderTraversal(self, root):
-        """
-        :type root: TreeNode
-        :rtype: List[int]
-        """
-        
-        # Recursion way
-#         res = []
-        
-#         def travel(node):
-#             if not node:
-#                 return 
-            
-#             res.append(node.val)
-#             travel(node.left)
-#             travel(node.right)
-        
-#         travel(root)
-#         return res
-        
-        # Iterative Solution
-        if not root:
-            return None
-        
+class Solution:
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        st = []
+        cur = root
         res = []
-        stack = []
-        stack.append(root)
         
-        while stack:
-            curr = stack.pop()
-            res.append(curr.val)
-            
-            if curr.right:
-                stack.append(curr.right)
-            if curr.left:
-                stack.append(curr.left)
-        
+        while st or cur:
+            while cur:
+                res.append(cur.val)
+                st.append(cur)
+                cur = cur.left
+            cur = st.pop().right
         return res
-        
-        
-        
-        
-        # Optimal Solution
-        
-        # res=[]
-        # stack=[]
-        # cur=root
-        # while cur or stack:
-        #     if cur:
-        #         res.append(cur.val)
-        #         stack.append(cur.right)
-        #         cur=cur.left
-        #     else:
-        #         cur=stack.pop()
-        # return res        
-            
-        
