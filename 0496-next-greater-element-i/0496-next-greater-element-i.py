13cf3163b427ee0a2806bc29bd34d04d7003e417
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-13 18:55:11 -0700

    Time: 48 ms (63.80%), Space: 16.7 MB (66.23%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.py
+++ 0496-next-greater-element-i/0496-next-greater-element-i.py
@@ -1,51 +1,18 @@
-class Solution(object):
-    def nextGreaterElement(self, nums1, nums2):
-        """
-        :type nums1: List[int]
-        :type nums2: List[int]
-        :rtype: List[int]
-        """
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         n = len(nums1)
-        res = [-1]*n
-        indexMap = {}
-        for i, n in enumerate(nums1):
-            indexMap[n] = i
+        result = [-1]*n
         stack = []
+        indexMap = {v : i for i, v in enumerate(nums1)}
         
-        for i in xrange(len(nums2) - 1, -1, -1):
-
-            while stack and nums2[i] > stack[-1]:
+        for i in range(len(nums2) - 1, -1, -1):
+            num = nums2[i]
+            
+            while stack and stack[-1] < num:
                 stack.pop()
-            
-            if nums2[i] in indexMap:
-                if stack:
-                    res[indexMap[nums2[i]]] = stack[-1]        
-            
-            stack.append(nums2[i])
+            if num in indexMap and stack:
+                index = indexMap[num]
+                result[index] = stack[-1]
+            stack.append(num)
         
-        return res
-                
-                
-        
-        
-        
-        
-        
-#         num1idx = {n:i for i,n in enumerate(nums1)}
-#         res = [-1]*len(nums1)
-#         stack = []
-        
-#         for i in xrange(len(nums2)-1, -1,-1):
-#             curr = nums2[i]
-            
-#             while stack and stack[-1] <= curr:
-#                 stack.pop()
-#             if curr in num1idx:
-#                 if stack:
-#                     res[num1idx[curr]] = stack[-1]
-#                 else:
-#                     res[num1idx[curr]] = -1
-            
-#             stack.append(curr)
-        
-#         return res
+        return result
