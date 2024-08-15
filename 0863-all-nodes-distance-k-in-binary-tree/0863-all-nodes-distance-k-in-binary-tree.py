338856036e9da7363f33010b01b84e4a11a7a17b
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-14 23:08:53 -0700

    Time: 27 ms (12.06%), Space: 11.8 MB (97.46%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.py
@@ -6,26 +6,13 @@
 #         self.right = None
 
 class Solution(object):
-    
-    
-    
-    
-    
-    def adjecency(self, root, parentMap):
-        q= deque()
-        q.append(root)
-        
-        while q:
-            node =q.popleft()
+    def _addParent(self, node, parent):
+        if node:
+            node.parent = parent
+            self._addParent(node.left, node)
+            self._addParent(node.right, node)
             
-            if node.left:
-                parentMap[node.left] = node
-                q.append(node.left)
-            if node.right:
-                parentMap[node.right] = node
-                q.append(node.right)        
-        
-    def distanceK(self, root, target, K):
+    def distanceK(self, root, target, k):
         """
         :type root: TreeNode
         :type target: TreeNode
@@ -33,53 +20,71 @@ class Solution(object):
         :rtype: List[int]
         """
         
-        parent_map = {}
-        def _preorder(node, parent):
-            if node:
-                parent_map[node] = parent
-                _preorder(node.left, node)
-                _preorder(node.right, node)
-        _preorder(root, None)
-        if target not in parent_map:
-            return []
-        ## BFS
-        dq = deque([(target, 0)])
-        visited = {target}
-        while dq:
-            node, dist = dq.popleft()
-            if dist == K:
-                return [node.val] + [n.val for n, d in dq] 
-            for nei in [node.left, node.right, parent_map[node]]:
-                if nei and nei not in visited:
-                    dq.append((nei, dist + 1))
-                    visited.add(nei)
-        return []
+        res = []
+        visited = set()
+        self._addParent(root, None)
+        self.helper(target, k, res, visited)
+        return res
+        
+    def helper(self, node, distance, arr, visited):
+        if not node or node in visited:
+            return
+        if distance == 0:
+            arr.append(node.val)
+        
+        visited.add(node)
+        
+        self.helper(node.parent, distance - 1, arr, visited)
+        self.helper(node.left, distance - 1, arr, visited)
+        self.helper(node.right, distance - 1, arr, visited)
+        
+#         parent_map = {}
+#         def _preorder(node, parent):
+#             if node:
+#                 parent_map[node] = parent
+#                 _preorder(node.left, node)
+#                 _preorder(node.right, node)
+#         _preorder(root, None)
+#         if target not in parent_map:
+#             return []
+#         ## BFS
+#         dq = deque([(target, 0)])
+#         visited = {target}
+#         while dq:
+#             node, dist = dq.popleft()
+#             if dist == K:
+#                 return [node.val] + [n.val for n, d in dq] 
+#             for nei in [node.left, node.right, parent_map[node]]:
+#                 if nei and nei not in visited:
+#                     dq.append((nei, dist + 1))
+#                     visited.add(nei)
+#         return []
         
         
         
         
         
-        parentMap = {}
-        self.adjecency(root,parentMap)
+#         parentMap = {}
+#         self.adjecency(root,parentMap)
         
     
-        q = deque()
-        q.append([target,0])
-        visited = set([target])
-        res = []
+#         q = deque()
+#         q.append([target,0])
+#         visited = set([target])
+#         res = []
         
-        while q:
-            node, dist = q.popleft()
+#         while q:
+#             node, dist = q.popleft()
             
-            if dist == k:
-                res.append(node.val)
+#             if dist == k:
+#                 res.append(node.val)
             
-            for neighbor in [node.left, node.right, parentMap.get(node)]:
-                if neighbor and neighbor not in visited:
-                    q.append([neighbor, dist + 1])
-                    visited.add(neighbor)
+#             for neighbor in [node.left, node.right, parentMap.get(node)]:
+#                 if neighbor and neighbor not in visited:
+#                     q.append([neighbor, dist + 1])
+#                     visited.add(neighbor)
         
-        return res  
+#         return res  
     
     
 #     def distanceK(self, root, target, k):
