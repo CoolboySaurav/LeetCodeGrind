16b89546e15e0605ef0db9b43160560af50e38a6
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-15 18:28:55 -0700

    Time: 31 ms (97.33%), Space: 18.4 MB (50.06%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -1,31 +1,15 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def isValidBST(self, root):
-        """
-        :type root: TreeNode
-        :rtype: bool
-        """
-        
-        def validate(node, minVal, maxVal):
-            if not node:
-                return True
-            if node.val >= maxVal or node.val <= minVal:
-                return False
-            return validate(node.left, minVal, node.val) and validate(node.right, node.val, maxVal)
-        
-        return validate(root, float("-inf"), float("inf"))
-        
-        
-#         def fact(node,left,right):
-#             if not node:
-#                 return True
-#             if not ((node.val>left and node.val<right)):
-#                 return False
-#             return (fact(node.left,left,node.val) and fact(node.right,node.val,right))
-        
-#         return fact(root,float("-inf"),float("inf"))
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        if not root: return True
+        return self.helper(root, float('-inf'), float('inf'))
+    def helper(self, node, leftBound, rightBound):
+        if not node: return True
+        if node.val <= leftBound or node.val >= rightBound:
+            return False
+        return self.helper(node.left, leftBound, node.val) and self.helper(node.right, node.val, rightBound)
