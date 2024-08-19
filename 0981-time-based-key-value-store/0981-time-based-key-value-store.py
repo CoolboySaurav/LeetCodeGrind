6735fb6e71eac2dd8fb488a4f97d1a72442a1582
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-18 23:48:21 -0700

    Time: 489 ms (97.76%), Space: 73.7 MB (61.84%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.py
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.py
@@ -1,44 +1,34 @@
-class TimeMap(object):
+class TimeMap:
 
     def __init__(self):
-        self.timeMap=defaultdict(list)  
-        self.timeValue=defaultdict()  
+        self.keyTime = defaultdict(list)
+        self.timeVal = defaultdict(str)
 
-    def set(self, key, value, timestamp):
-        """
-        :type key: str
-        :type value: str
-        :type timestamp: int
-        :rtype: None
-        """
-        self.timeMap[key].append(timestamp)
-        self.timeValue[timestamp]=value
-        
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.keyTime[key].append(timestamp)
+        self.timeVal[timestamp] = value
 
-    def get(self, key, timestamp):
-        """
-        :type key: str
-        :type timestamp: int
-        :rtype: str
-        """
-        arr=self.timeMap[key]
-        if not arr or timestamp<arr[0]:
+    def get(self, key: str, timestamp: int) -> str:
+        arr = self.keyTime[key]
+        if not arr or timestamp < arr[0]:
             return ""
-        elif timestamp>arr[-1]:
-            return self.timeValue[arr[-1]]
-        ans=l=0
-        r=len(arr)-1
-        while l<=r:
-            m=(l+r)//2
-            if arr[m]==timestamp:
-                return self.timeValue[arr[m]]
-            if arr[m]>=timestamp:
-                ans=m-1
-                r=m-1
+        elif timestamp > arr[-1]:
+            return self.timeVal[arr[-1]]
+        l, r = 0, len(arr) - 1
+        ans = r
+        
+        while l <= r:
+            mid = l + (r - l) // 2
+            if arr[mid] == timestamp:
+                return self.timeVal[arr[mid]]
+            elif arr[mid] <= timestamp:
+                ans = mid
+                l = mid + 1
             else:
-                l=m+1
-        return self.timeValue[arr[ans]]
-
+                r = mid - 1
+        return self.timeVal[arr[ans]]
+        
+        
 
 # Your TimeMap object will be instantiated and called as such:
 # obj = TimeMap()
