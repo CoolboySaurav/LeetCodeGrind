865a06dddc9223f3fcb66799abbe3ba754d85965
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-18 18:55:53 -0700

    Time: 51 ms (87.06%), Space: 16.6 MB (56.59%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,56 +1,25 @@
-
-#         cntr, w = Counter(s1), len(s1)   
-
-#         for i in range(len(s2)):
-#             if s2[i] in cntr: 
-#                 cntr[s2[i]] -= 1
-#             if i >= w and s2[i-w] in cntr: 
-#                 cntr[s2[i-w]] += 1
-
-#             if all([cntr[i] == 0 for i in cntr]): 
-#                 return True
-
-#         return False
-class Solution(object):
-    def checkInclusion(self, s1, s2):
-        """
-        :type s1: str
-        :type s2: str
-        :rtype: bool
-        """
-        if len(s1) > len(s2):
-            return False
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        if len(s1) > len(s2): return False
         
-        s1Count, s2Count = [0] * 26, [0] * 26
+        s1Map = defaultdict(int)
+        s2Map = defaultdict(int)
+        for c in s1:
+            s1Map[c] += 1 
         
-        for i in range(len(s1)):
-            s1Count[ord(s1[i]) - ord('a')] += 1
-            s2Count[ord(s2[i]) - ord('a')] += 1
+        l, r = 0, 0 
         
-        matches = 0
-        for i in range(26):
-            if s1Count[i] == s2Count[i]:
-                matches += 1
-        
-        l = 0
-        for r in range(len(s1), len(s2)):
-            if matches == 26:
-                return True
+        while r < len(s2):
             
-            index = ord(s2[r]) - ord('a')
-            s2Count[index] += 1
-            if s2Count[index] == s1Count[index]:
-                matches += 1
-            elif s2Count[index] == s1Count[index] + 1:
-                matches -= 1
+            s2Map[s2[r]] += 1
             
-            index = ord(s2[l]) - ord('a')
-            s2Count[index] -= 1
-            if s2Count[index] == s1Count[index]:
-                matches += 1
-            elif s2Count[index] == s1Count[index] - 1:
-                matches -= 1
-            
-            l += 1
+            if (r - l + 1) == len(s1):
+                if s2Map == s1Map:
+                    return True
+                s2Map[s2[l]] -= 1
+                if s2Map[s2[l]] == 0:
+                    del s2Map[s2[l]]
         
-        return matches == 26
+                l += 1
+            r += 1
+        return False
