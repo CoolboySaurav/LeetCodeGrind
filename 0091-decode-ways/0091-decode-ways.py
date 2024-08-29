89417f4bf14691871798b2e6f860319f09e3f932
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-08-28 18:25:06 -0700

    Time: 77 ms (6.59%), Space: 16.4 MB (80.27%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,6 +1,25 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
+        dp = [0] * (n + 1)
+        dp[n] = 1
+        
+        for i in range(n - 1, -1, -1):
+            count =  0
+            
+            for j in range(i, n):
+                p = s[i:j + 1]
+                
+                if 0 < int(p) <= 26 and int(p[0]) != 0:
+                    count += dp[j + 1]
+                
+            dp[i] = count
+        
+        return dp[0]
+        
+        
+        
+        n = len(s)
         memo = {}
         def helper(ind):
             if ind == n:
