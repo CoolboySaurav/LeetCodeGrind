e813ea0b72bc6edbced0a817558979995c3f6871
Saurav Sharad Mestry <143134730+CoolboySaurav@users.noreply.github.com>
2024-09-02 14:29:06 -0700

    Time: 1197 ms (12.07%), Space: 55.2 MB (53.32%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.py
@@ -5,15 +5,13 @@ class Solution:
         if the second interval start is in between the first interval, then we will remove the one that ends last. Doing so, we 
         made sure that the interval that ends earliest will be less likely to have any overlap with upcoming intervals
         '''
-        intervals.sort( key = lambda x : x[0])
-        interval = intervals[0]
+        intervals.sort()
+        prevEnd = intervals[0][1]
         count = 0
-        for i in range(1, len(intervals)):
-            a = intervals[i]
-            if interval[1] > a[0]: # We have overlap
+        for start, end in intervals[1:]:
+            if start >= prevEnd:
+                prevEnd = end
+            else:
                 count += 1
-                if interval[1] > a[1]:
-                    interval = a
-            else: # No overlap
-                interval = a
+                prevEnd = min(prevEnd, end)
         return count
